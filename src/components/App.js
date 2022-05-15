3a1975f587518884845634c25e99d3b8b6b05207
alwlsTm <mijine0716@naver.com>
2022-05-15 17:12:47 +0900

    20220515 음식 최신순/칼로리순 정렬 선택 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import items from '../mock.json';
 import FoodList from './FoodList';
 
 function App() {
+  const [order, setOrder] = useState('createdAt');  //아이템 정렬 state
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);  //아이템 정렬(내림차순)
+
+  const handleNewestClick = () => setOrder('createdAt');  //최신순
+
+  const handleCalorieClick = () => setOrder('calorie');   //칼로리순
+
   return (
     <div>
-      <FoodList items={items} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} />
     </div>
   );
 }
