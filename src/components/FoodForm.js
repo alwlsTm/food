aa4f926d83b14203d6e328a2d8b34f4a414d75ff
alwlsTm <mijine0716@naver.com>
2022-05-25 20:54:14 +0900

    20220525 음식 이미지 파일 인풋 추가

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import FileInput from "./FileInput";
 import './FoodForm.css';
 
 function FoodForm() {
@@ -8,26 +9,32 @@ function FoodForm() {
     title: '',
     calorie: 0,
     content: '',
+    imgFile: null,
   });
 
-  const handleChange = (e) => { //onChange
-    const { name, value } = e.target;
+  const handleChange = (name, value) => {
     setValues((prevValues) => ({
       ...prevValues,
-      [name]: value,
+      [name]: value,  //name으로 프로퍼티 명을 지정, value로 해당하는 값을 지정
     }));
   };
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  };
+
   const handleSubmit = (e) => { //onSubmit
-    e.preventDefault(); //HTML 폼의 기본 동작을 막음
+    e.preventDefault();   //HTML 폼의 기본 동작을 막음
     console.log(values);
   }
 
   return (
     <form className="FoodForm" onSubmit={handleSubmit}>
-      <input name="title" value={values.title} onChange={handleChange}></input>
-      <input type="number" name="calorie" value={values.calorie} onChange={handleChange}></input>
-      <input name="content" value={values.content} onChange={handleChange}></input>
+      <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
+      <input name="title" value={values.title} onChange={handleInputChange}></input>
+      <input type="number" name="calorie" value={values.calorie} onChange={handleInputChange}></input>
+      <input name="content" value={values.content} onChange={handleInputChange}></input>
       <button type="submit">확인</button>
     </form>
   );
