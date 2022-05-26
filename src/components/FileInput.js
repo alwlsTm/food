bc7c2e2e56e21d1b237bf05de8e90461109b2591
alwlsTm <mijine0716@naver.com>
2022-05-26 23:16:53 +0900

    20220526 음식 이미지 파일 초기화 추가

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,11 +1,26 @@
+import { useRef } from "react";
+
 function FileInput({ name, value, onChange }) {  //file - 비제어 컴포넌트(value prop 사용X)
+  const inputRef = useRef();  //ref 객체(실제 DOM 노드를 참조)
+
   const handleChange = (e) => {
-    const nextValue = e.target.files[0];
-    onChange(name, nextValue);
+    const nextValue = e.target.files[0];  //file[0] - 선택한 이미지 파일의 객체
+    onChange(name, nextValue);  //imgFile: 이미지 파일 객체
+  };
+
+  const handleClearClick = () => {  //초기화
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = '';
+    onChange(name, null); //imgFile: null
   };
 
   return (
-    <input type="file" onChange={handleChange}></input>
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef}></input>
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
   );
 }
 
